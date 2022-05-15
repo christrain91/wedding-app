0a5715e70cfaca042978497a586f7542981745e6
Christopher Train <ctrain@pcs-publishing.com>
2022-05-15 17:16:57 +0100

    Pull from environment variable to setup frontend

--- lib/FrontendStack.ts
+++ lib/FrontendStack.ts
@@ -12,11 +12,9 @@ export default class WebsiteStack extends Stack {
     }
 
     const environment = {
-      NEXT_PUBLIC_SUPABASE_URL: 'https://kynipcbxmydblnjghgqc.supabase.co',
-      NEXT_PUBLIC_SUPABASE_KEY:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5bmlwY2J4bXlkYmxuamdoZ3FjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTEzMDQyMTMsImV4cCI6MTk2Njg4MDIxM30.v0UjBTGnyIHr0jIx1jUsnrTwljwC_MFWEyavQNoVvz4',
-      SUPABASE_SERVICE_LEVEL_KEY:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5bmlwY2J4bXlkYmxuamdoZ3FjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1MTMwNDIxMywiZXhwIjoxOTY2ODgwMjEzfQ.7K59sc_V9UiFyASVyZQv-YEsw_jc8XNhws4vfX146qE'
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+      SUPABASE_SERVICE_LEVEL_KEY: process.env.SUPABASE_SERVICE_LEVEL_KEY
     }
 
     const api = new Api(this, 'Api', {
